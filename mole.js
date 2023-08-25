efd348274c60b7f86213e94396701277ceb39da6
noviceCoder0324 <88302674+noviceCoder0324@users.noreply.github.com>
2023-08-25 14:17:35 -0500

    add pipes

--- mole.js
+++ mole.js
@@ -1,1 +1,13 @@
-console.log('something');
+window.onload = () => {
+    setGame();
+}
+
+function setGame() {
+    //set up the grid for the game board in html
+    for (let i = 0; i < 9; i++) {
+        //create a <div id="0-8"></div> in tile
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+}
